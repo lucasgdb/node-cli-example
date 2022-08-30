f995d6a80362957b3219f17bb364d1d404cb5545
lucasgdb <lucasgdbittencourt@gmail.com>
2022-08-30 16:25:13 -0300

    first commit

---
+++ src/cli.ts
@@ -0,0 +1,77 @@
+import arg from "arg";
+import inquirer from "inquirer";
+
+import { createProject } from "./main";
+
+type Options = {
+  skipPrompts: boolean;
+  git: boolean;
+  template: string;
+  runInstall: boolean;
+};
+
+async function promptForMissingOptions(options: Options) {
+  const defaultTemplate = "JavaScript";
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate,
+    };
+  }
+
+  const questions = [];
+  if (!options.template) {
+    questions.push({
+      type: "list",
+      name: "template",
+      message: "Please choose which project template to use",
+      choices: ["JavaScript", "TypeScript"],
+      default: defaultTemplate,
+    });
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: "confirm",
+      name: "git",
+      message: "Initialize a git repository?",
+      default: false,
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answers.template,
+    git: options.git || answers.git,
+  };
+}
+
+function parseArgumentsIntoOptions(rawArgs: string[]) {
+  const args = arg(
+    {
+      "--git": Boolean,
+      "--yes": Boolean,
+      "--install": Boolean,
+      "-g": "--git",
+      "-y": "--yes",
+      "-i": "--install",
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+
+  return promptForMissingOptions({
+    skipPrompts: args["--yes"] || false,
+    git: args["--git"] || false,
+    template: args._[0],
+    runInstall: args["--install"] || false,
+  });
+}
+
+export async function cli(args: string[]) {
+  const options = await parseArgumentsIntoOptions(args);
+
+  await createProject(options);
+}
